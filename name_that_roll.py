9becd152c647fab9ca6ae48ad3395d10ee306ecb
Student <student@ent-v321-011.local>
2013-05-15 18:23:47 -0400

    did name that roll

---
+++ name_that_roll.py
@@ -0,0 +1,40 @@
+"""
+name_that_roll.py
+=====
+Write a program that names the rolls of two dice in a dice game called craps...
+
+* ask the user for the values of two dice rolls.  
+* output the name of the roll using Wikipedia's article on Craps
+	* http://en.wikipedia.org/wiki/Craps#Rolling
+* only check for the following rolls:
+	* _Snake Eyes_
+	* _Hard Four_ 
+	* _Easy Four_  
+* print out "I don't know yet" for any other rolls.  Example output:
+* example interaction:
+
+What roll did you get for the first die?
+> 1
+What roll did you get for the second die?
+> 1
+Snake Eyes!
+
+Press ENTER or type command to continue
+What roll did you get for the first die?
+> 1
+What roll did you get for the second die?
+> 3
+Easy Four
+"""
+
+udice1 = raw_input("What was the first dice roll?\n> ")
+udice2 = raw_input("What was the second dice roll?\n> ")
+
+if (udice1, udice2) == ("1","1"):
+	print("Snake Eyes!")
+elif (udice1, udice2) == ("2", "2"):
+	print("Hard Four!")
+elif (udice1, udice2) in [("3", "1"), ("1", "3")]:
+	print("Easy Four!")
+else:
+	print("I don't know yet.")
